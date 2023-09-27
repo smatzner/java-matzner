51cdcd0bf73fbc618a3e9198d452a40a013182a1
smatzner <stefanmatzner@gmx.at>
2023-09-27 14:03:49 +0200

    created Article and User sets in Main Class (task 6)

--- controllerAufgabe/src/main/java/com/example/controlleraufgabe/ControllerAufgabeApplication.java
+++ controllerAufgabe/src/main/java/com/example/controlleraufgabe/ControllerAufgabeApplication.java
@@ -1,13 +1,20 @@
 package com.example.controlleraufgabe;
 
+import com.example.controlleraufgabe.entity.Article;
+import com.example.controlleraufgabe.entity.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @SpringBootApplication
 public class ControllerAufgabeApplication {
+    public Set<User> users = new HashSet<User>();
+    public Set<Article> articles = new HashSet<Article>();
 
     public static void main(String[] args) {
-        SpringApplication.run(ControllerAufgabeApplication.class, args);
+                SpringApplication.run(ControllerAufgabeApplication.class, args);
     }
 
 }
