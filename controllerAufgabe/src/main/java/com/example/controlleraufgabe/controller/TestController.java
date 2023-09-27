33a2f2e06c9020c372f2159927af2f687352b6b7
smatzner <stefanmatzner@gmx.at>
2023-09-27 13:12:03 +0200

    created TestController Class (task 2) and added GET Request Method (task 3.1)

--- controllerAufgabe/src/main/java/com/example/controlleraufgabe/controller/TestController.java
+++ controllerAufgabe/src/main/java/com/example/controlleraufgabe/controller/TestController.java
@@ -1,17 +1,17 @@
 package com.example.controlleraufgabe.controller;
 
 import lombok.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 
 @RestController
 @RequestMapping("api/test")
 public class TestController {
-    
+
+    @GetMapping
+    public String printMyFirstRequest(){
+        return "Mein erster Request";
+    }
 }
