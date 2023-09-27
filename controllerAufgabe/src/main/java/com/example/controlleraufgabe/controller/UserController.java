c148c4bbfc85ddfacb8e6b453a9ef0c3f9cf0853
smatzner <stefanmatzner@gmx.at>
2023-09-27 15:13:31 +0200

    added UserDTO and registerUSer methon in UserController (task 8.5)

--- controllerAufgabe/src/main/java/com/example/controlleraufgabe/controller/UserController.java
+++ controllerAufgabe/src/main/java/com/example/controlleraufgabe/controller/UserController.java
@@ -1,5 +1,10 @@
 package com.example.controlleraufgabe.controller;
 
+import com.example.controlleraufgabe.ControllerAufgabeApplication;
+import com.example.controlleraufgabe.dto.UserDTO;
+import com.example.controlleraufgabe.entity.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/user")
 public class UserController {
 
+    @PostMapping
+    public String registerUser(@RequestBody UserDTO userDTO){
+        for(User user : ControllerAufgabeApplication.users){
+            if(user.getUserId() == userDTO.getUserId()){
+                return "User bereits vorhanden!";
+            }
+        }
+        User user = new User(
+                userDTO.getUserId(),
+                userDTO.getUsername(),
+                userDTO.getPassword(),
+                userDTO.getAge()
+        );
+
+        ControllerAufgabeApplication.users.add(user);
+        return "User erfolgriech angelegt";
+    }
 }
