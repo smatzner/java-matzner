11b7e282c7892dcbcd9c2351179dcf370fd9981d
smatzner <stefanmatzner@gmx.at>
2023-10-12 11:15:51 +0200

    created purchases hashmap (task 5)

--- serviceAufgabe/src/main/java/com/example/serviceaufgabe/ServiceAufgabeApplication.java
+++ serviceAufgabe/src/main/java/com/example/serviceaufgabe/ServiceAufgabeApplication.java
@@ -5,13 +5,16 @@ import com.example.serviceaufgabe.entities.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 @SpringBootApplication
 public class ServiceAufgabeApplication {
     public static Set<User> users = new HashSet<User>();
     public static Set<Article> articles = new HashSet<Article>();
+    public static Map<User, Article> purchases = new HashMap<>();
 
     public static void main(String[] args) {
         SpringApplication.run(ServiceAufgabeApplication.class, args);
